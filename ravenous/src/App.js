7db1421359bf7c05eefba9a8f18979a450909895
crileyy <criley0007@gmail.com>
2018-08-28 22:23:14 -0700

    Added components to main component

--- ravenous/src/App.js
+++ ravenous/src/App.js
@@ -1,20 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Business from './components/Business/Business.js';
+import BusinessList from './components/Business/BusinessList/BusinessList.js';
+import SearchBar from './components/Business/SearchBar/SearchBar.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Restaurant Finder!</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Business />
+        <h1>Restaurant Finder</h1>
+        <SearchBar />
+        <BusinessList />
       </div>
     );
   }
