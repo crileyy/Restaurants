5df85f845d95442e1dc5817807c5f3b767fa1aa1
crileyy <criley0007@gmail.com>
2018-08-28 22:36:16 -0700

    Updated BusinessList component logic

--- ravenous/src/App.js
+++ ravenous/src/App.js
@@ -4,13 +4,34 @@ import './App.css';
 import BusinessList from './components/Business/BusinessList/BusinessList.js';
 import SearchBar from './components/Business/SearchBar/SearchBar.js';
 
+const business = {
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+}
+
+const businesses = [
+  business,
+  business,
+  business,
+  business,
+  business,
+  business
+];
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Restaurant Finder</h1>
         <SearchBar />
-        <BusinessList />
+        <BusinessList businesses={businesses}/>
       </div>
     );
   }
